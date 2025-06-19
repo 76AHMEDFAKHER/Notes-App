9f9e56cd7646890550f24a90e06066f88c2c6d4d
76AHMEDFAKHER <220210469@fa-hists.edu.eg>
2025-06-19 17:29:43 +0300

    initial project

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/notes_home.dart';
+
+void main() {
+  runApp(NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Notes App', home: NotesHome());
+  }
+}
